fe01578467987dff2a99ffc2a7db2852abe15e2f
Nate Bosch <nbosch1@gmail.com>
2016-12-06 22:54:20 -0800

    More interesting client interactions
    - Add 'random_ints' method that read's parameters and returns different
      data each time. Can be used to generate large responses
    - Add methods to start and stop periodic notifications
    - Change the fallback to print out the method and parameters it got

--- bin/demo_server.dart
+++ bin/demo_server.dart
@@ -1,19 +1,41 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:json_rpc_2/json_rpc_2.dart';
 
 import 'package:dart_language_server/dart_language_server.dart';
 
-void main() {
+final random = new Random();
+
+void main() async {
   var logFile = new File('/tmp/wirelog.txt');
   var channel = new StdIOStreamChannel();
   var logged = wireLog(channel, logFile);
+  Timer timer;
   var server = new Peer(logged);
-  server.registerMethod('foo', (params) {
-    return params.asList;
-  });
-  server.registerFallback(([params]) {
-    return 'fallback';
-  });
-  server.listen();
+  server
+    ..registerMethod('random_ints', (params) {
+      return _randomInts(params['count'].value).toList();
+    })
+    ..registerMethod('start_notifications', () {
+      timer ??= new Timer.periodic(const Duration(seconds: 3), (_) {
+        server.sendNotification('some_notification', _randomInts(2));
+      });
+    })
+    ..registerMethod('stop_notifications', () {
+      timer?.cancel();
+      timer = null;
+    })
+    ..registerFallback(([params]) {
+      return 'fallback for "${params.method}" with params ${params.value}';
+    });
+  await server.listen();
+  timer?.cancel();
+}
+
+Iterable<int> _randomInts(int count) sync* {
+  for (var i = 0; i < count; i++) {
+    yield random.nextInt(100);
+  }
 }

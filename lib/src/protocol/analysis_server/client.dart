81f4e6fdc99a37fa7df302c63851efb10ea82b54
Nate Bosch <nbosch1@gmail.com>
2017-01-16 15:01:09 -0800

    Add an implementation of AnalysisServer
    - Add RpcClient to handle the wireformat of the AnalysisServer which is
      very similar to json-rpc but doesn't not quite match the
      implementation in the json_rpc_2 package:
      - Requests don't include the "jsonrpc" key
      - IDs *must* be a String. json_rpc_2 uses ints
    - Add SubprocessAnalysisServer which starts the sdk analysis server and
      translates requests as needed.

---
+++ lib/src/protocol/analysis_server/client.dart
@@ -0,0 +1,66 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+
+import 'interface.dart';
+import 'messages.dart';
+import 'wireformat.dart';
+
+String get _dart => Platform.resolvedExecutable;
+String get _analysisServer =>
+    '${p.dirname(_dart)}/snapshots/analysis_server.dart.snapshot';
+
+/// An [AnalysisServer] which is a client of the sdk provided analysis server
+/// as a subprocess.
+class SubprocessAnalysisServer implements AnalysisServer {
+  final RpcClient _client;
+
+  static Future<AnalysisServer> start([String wirelogPath]) async {
+    var process = await Process.start(_dart, [_analysisServer]);
+    var client = new RpcClient('AnalysisServer', process, wirelogPath);
+    return new SubprocessAnalysisServer._(client);
+  }
+
+  SubprocessAnalysisServer._(this._client) {
+    _notifications();
+  }
+
+  void _notifications() {
+    _client.registerEventHandler('analysis.errors', (params) {
+      _analysisErrors.add(new AnalysisErrors.fromJson(params));
+    });
+  }
+
+  @override
+  Stream<AnalysisErrors> get analysisErrors => _analysisErrors.stream;
+  final _analysisErrors = new StreamController<AnalysisErrors>();
+
+  @override
+  Future<Null> shutdown() async {
+    await _client.sendRequest('server.shutdown');
+  }
+
+  @override
+  Future<Null> setPriorityFiles(List<String> files) async {
+    await _client.sendRequest('analysis.setPriorityFiles', {'files': files});
+  }
+
+  @override
+  Future<Null> setAnalysisRoots(List<String> included, List<String> excluded,
+      [Map<String, String> packageRoots]) async {
+    await _client.sendRequest('analysis.setAnalysisRoots', {
+      'included': included,
+      'excluded': excluded,
+      'packageRoots': packageRoots
+    });
+  }
+
+  @override
+  Future<Null> updateContent(Map<String, ContentOverlayUpdate> files) async {
+    var serializable =
+        new Map.fromIterable(files.keys, value: (file) => files[file].toJson());
+    await _client
+        .sendRequest('analysis.updateContent', {'files': serializable});
+  }
+}
---
+++ lib/src/protocol/analysis_server/wireformat.dart
@@ -0,0 +1,93 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:async/async.dart';
+import 'package:logging/logging.dart';
+import 'package:stream_channel/stream_channel.dart';
+
+import '../../utils/wirelog.dart';
+
+typedef void EventHandler(dynamic /*json serializable*/ params);
+
+/// A client communicating with an RPC server [Process] over std io streams.
+///
+/// This is similar to a JsonRPC client but does not include the "jsonrpc" key
+/// and ids are forced to be Strings.
+class RpcClient {
+  final StreamChannel<String> _channel;
+  final Logger _log;
+  final _outstandingRequests = <String, Completer>{};
+  final _eventHandlers = <String, EventHandler>{};
+
+  int _requestId = 0;
+
+  factory RpcClient(String name, Process process, [String wirelogPath]) {
+    final utf8Transformer = new StreamSinkTransformer.fromHandlers(
+        handleData: (String data, EventSink<List<int>> sink) {
+      sink.add(UTF8.encode(data));
+    });
+    var inStrings = utf8Transformer.bind(process.stdin);
+    var outStrings = process.stdout.map(UTF8.decode);
+    var channel = new StreamChannel(outStrings, inStrings);
+
+    if (wirelogPath != null) {
+      var channelLog = new File(wirelogPath).openWrite();
+      channel = wireLog(channel, channelLog);
+      process.exitCode.then((exitCode) {
+        channelLog.writeln('Analysis Server exited [$exitCode]');
+        channelLog.close();
+      });
+    }
+    return new RpcClient._(channel, new Logger('$name-rpc'));
+  }
+
+  RpcClient._(this._channel, this._log) {
+    _channel.stream.listen(_handleResponse);
+  }
+
+  void registerEventHandler(String eventName, EventHandler handler) {
+    _eventHandlers[eventName] = handler;
+  }
+
+  Future sendRequest(String method, [dynamic /*json serializable*/ params]) {
+    var id = '${_requestId++}';
+    var request = {'id': id, 'method': method};
+    if (params != null) {
+      request['params'] = params;
+    }
+    _channel.sink.add('${JSON.encode(request)}\n');
+    var completer = new Completer();
+    _outstandingRequests[id] = completer;
+    return completer.future;
+  }
+
+  void _handleResponse(String response) {
+    response = response.trim();
+    if (response.isEmpty) {
+      _log.fine('Got empty response');
+      return;
+    }
+    dynamic message;
+    try {
+      message = JSON.decode(response);
+    } catch (e) {
+      _log.severe('Error while decoding: $e');
+      return;
+    }
+    if (message.containsKey('id')) {
+      var id = message['id'];
+      _log.fine('Completing request: $id');
+      _outstandingRequests[id].complete(message);
+      _outstandingRequests.remove(id);
+    } else {
+      var event = message['event'];
+      _log.fine('Handling event: $event');
+      if (!_eventHandlers.containsKey(event)) {
+        _log.info('Ignoring event $event');
+        return;
+      }
+      _eventHandlers[event](message['params']);
+    }
+  }
+}

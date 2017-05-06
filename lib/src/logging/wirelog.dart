ecb9ea30db640f82cf54973be5a29811708545f0
Nate Bosch <nbosch@google.com>
2017-05-05 22:29:28 -0700

    Use `tap` from pkg:stream_transform for wirelog

--- lib/src/logging/wirelog.dart
+++ lib/src/logging/wirelog.dart
@@ -1,7 +1,6 @@
-import 'dart:async';
-
 import 'package:async/async.dart';
 import 'package:stream_channel/stream_channel.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 class WireLog {
   StringSink _log;
@@ -12,33 +11,10 @@ class WireLog {
 
   StreamChannelTransformer<String, String> _transformer;
   StreamChannelTransformer<String, String> get transformer =>
-      _transformer ?? _initTransformer();
+      _transformer ??
+      new StreamChannelTransformer(_tapLog('In'),
+          new StreamSinkTransformer.fromStreamTransformer(_tapLog('Out')));
 
-  StreamChannelTransformer<String, String> _initTransformer() {
-    var inTransformer = new StreamTransformer.fromHandlers(
-        handleData: _inData, handleDone: _inDone);
-    var outTransformer = new StreamSinkTransformer.fromHandlers(
-        handleData: _outData, handleDone: _outDone);
-    return new StreamChannelTransformer(inTransformer, outTransformer);
-  }
-
-  void _inData(String data, EventSink<String> sink) {
-    _log?.writeln('In: $data');
-    sink.add(data);
-  }
-
-  void _outData(String data, EventSink<String> sink) {
-    _log?.writeln('Out: $data');
-    sink.add(data);
-  }
-
-  void _inDone(EventSink<String> sink) {
-    _log?.writeln('In: CLosed');
-    sink.close();
-  }
-
-  void _outDone(EventSink<String> sink) {
-    _log?.writeln('Out: CLosed');
-    sink.close();
-  }
+  _tapLog(String prefix) => tap((data) => _log?.writeln('$prefix: $data'),
+      onDone: () => _log?.writeln('$prefix: Closed'));
 }

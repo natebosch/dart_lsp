421f33758522cf9e7374b5c374106107b04c7062
Nate Bosch <nbosch1@gmail.com>
2020-03-22 12:23:26 -0700

    Add default implementations for all methods

--- lib/src/protocol/language_server/interface.dart
+++ lib/src/protocol/language_server/interface.dart
@@ -5,46 +5,60 @@ import 'package:json_rpc_2/json_rpc_2.dart';
 import 'messages.dart';
 
 abstract class LanguageServer {
-  Future<void> get onDone;
+  final _onDone = Completer<void>();
+  Future<void> get onDone => _onDone.future;
 
   Future<void> shutdown() async {}
-  void exit() {}
+  void exit() {
+    _onDone.complete();
+  }
 
   Future<ServerCapabilities> initialize(int clientPid, String rootUri,
-      ClientCapabilities clientCapabilities, String trace);
-  void initialized();
+          ClientCapabilities clientCapabilities, String trace) async =>
+      ServerCapabilities((b) => b);
+  void initialized() {}
   void textDocumentDidOpen(TextDocumentItem document) {}
   void textDocumentDidChange(VersionedTextDocumentIdentifier documentId,
       List<TextDocumentContentChangeEvent> changes) {}
   void textDocumentDidClose(TextDocumentIdentifier documentId) {}
   Future<CompletionList> textDocumentCompletion(
-      TextDocumentIdentifier documentId, Position position);
+          TextDocumentIdentifier documentId, Position position) async =>
+      CompletionList((b) => b);
   Future<Location> textDocumentDefinition(
-      TextDocumentIdentifier documentId, Position position);
+          TextDocumentIdentifier documentId, Position position) async =>
+      null;
   Future<List<Location>> textDocumentReferences(
-      TextDocumentIdentifier documentId,
-      Position position,
-      ReferenceContext context);
+          TextDocumentIdentifier documentId,
+          Position position,
+          ReferenceContext context) async =>
+      [];
   Future<List<Location>> textDocumentImplementation(
-      TextDocumentIdentifier documentId, Position position);
+          TextDocumentIdentifier documentId, Position position) async =>
+      [];
   Future<List<DocumentHighlight>> textDocumentHighlight(
-      TextDocumentIdentifier documentId, Position position);
+          TextDocumentIdentifier documentId, Position position) async =>
+      [];
   Future<List<SymbolInformation>> textDocumentSymbols(
-      TextDocumentIdentifier documentId);
-  Future<List<SymbolInformation>> workspaceSymbol(String query);
+          TextDocumentIdentifier documentId) async =>
+      [];
+  Future<List<SymbolInformation>> workspaceSymbol(String query) async => [];
   Future<dynamic> textDocumentHover(
-      TextDocumentIdentifier documentId, Position position);
+          TextDocumentIdentifier documentId, Position position) async =>
+      null;
   Future<List<dynamic /*Command|CodeAction*/ >> textDocumentCodeAction(
-      TextDocumentIdentifier documentId,
-      Range range,
-      CodeActionContext context);
-  Future<void> workspaceExecuteCommand(String command, List<dynamic> arguments);
-  Future<WorkspaceEdit> textDocumentRename(
-      TextDocumentIdentifier documentId, Position position, String newName);
-  Stream<Diagnostics> get diagnostics;
-  Stream<ApplyWorkspaceEditParams> get workspaceEdits;
-  Stream<ShowMessageParams> get showMessages;
-  Stream<ShowMessageParams> get logMessages;
+          TextDocumentIdentifier documentId,
+          Range range,
+          CodeActionContext context) async =>
+      [];
+  Future<void> workspaceExecuteCommand(
+      String command, List<dynamic> arguments) async {}
+  Future<WorkspaceEdit> textDocumentRename(TextDocumentIdentifier documentId,
+          Position position, String newName) async =>
+      null;
+  Stream<Diagnostics> get diagnostics => Stream.empty();
+  Stream<ApplyWorkspaceEditParams> get workspaceEdits => Stream.empty();
+  Stream<ShowMessageParams> get showMessages => Stream.empty();
+  Stream<ShowMessageParams> get logMessages => Stream.empty();
 
   void setupExtraMethods(Peer peer) {}
 }
